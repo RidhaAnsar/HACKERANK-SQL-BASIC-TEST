3db12a6bea1f38013140380c3e49f429f3b4179d
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-07-15 14:41:21 +0530

    profitable-stocks.sql

---
+++ profitable-stocks.sql
@@ -0,0 +1,1 @@
+SELECT price_today.stock_code FROM price_today INNER JOIN price_tomorrow ON price_today.stock_code = price_tomorrow.stock_code WHERE price_tomorrow.price>price_today.price ORDER BY stock_code asc;
